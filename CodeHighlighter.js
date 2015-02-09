c0202a91b6436096941d806cffd63d4aa9f6be2b
Michael David Kuckuk <mdk@kuckuk.com>
2015-02-09 11:19:14 +0100

    Update CodeHighlighter.js

--- CodeHighlighter.js
+++ CodeHighlighter.js
@@ -1,4 +1,21 @@
-/* Copyright (c) 2015 Michael David Kuckuk,  mdk(a)kuckuk.com */
+/*
+ * CodeHighlighter - a JavaScript library to highlight Code as in Notepad++
+ * Copyright (C) 2015  Michael David Kuckuk
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 $.fn.highlightCode = function(codeType) {
 	var self = this;
